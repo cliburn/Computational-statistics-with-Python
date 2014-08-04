03717301b3d82414f7a30442f62387099627d681
Cliburn Chan <cliburn.chan@duke.edu>
2014-08-04 14:39:16 -0400

    add parakeet

--- lessons/pdist_cpp.cpp
+++ lessons/pdist_cpp.cpp
@@ -2,7 +2,9 @@
 
 extern "C" 
 
-void pdist_cpp(int n, int p, double xs[n*p], double D[n*n]) {
+// Variable length arrays are OK for C99 but not legal in C++
+// void pdist_cpp(int n, int p, double xs[n*p], double D[n*n]) {
+void pdist_cpp(int n, int p, double *xs, double *D) {
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
             double s = 0.0;
