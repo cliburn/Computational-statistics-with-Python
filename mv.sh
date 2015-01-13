601a4851e1e61efc53e3b416aee6cf17a8f2c6a2
Janice McCarthy <janice@duke.edu>
2015-01-13 08:58:12 -0500

    Linear Algebra - still in progress

---
+++ mv.sh
@@ -0,0 +1,6 @@
+mv lessons/SettingUpWorkspace.ipynb lessons/SettingUpWorkspace_hold.ipynb
+mv lessons/"Testing and Debugging".ipynb lessons/"Testing and Debugging_hold".ipynb
+mv lessons/Untitled1.ipynb lessons/Untitled1_hold.ipynb
+mv lessons/VersionControlWithGit-Copy0.ipynb lessons/VersionControlWithGit-Copy0_hold.ipynb
+mv lessons/VersionControlWithGit.ipynb lessons/VersionControlWithGit_hold.ipynb
+mv lessons/hello.c lessons/hello_hold.c
